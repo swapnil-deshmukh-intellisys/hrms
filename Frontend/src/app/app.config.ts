51b4a077fb574c390aace14bc38055688511d035
ritesh-intellisys <ritesh.intellisys@gmail.com>
2025-12-15 11:08:12 +0530

    chore: configure application settings

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,18 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
 
-import { routes } from './app.routes'; // ✅ Ensure this path is correct
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptorsFromDi(), withFetch()) // ✅ Added HttpClient with Fetch API
+    provideHttpClient(withInterceptorsFromDi(), withFetch())
   ]
 };
